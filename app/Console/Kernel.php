d0f711525da76486e847d94ca3db38c48dbe748b
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-01-30 12:49:08 -0500

    Implement backups (#7)
    * Added `spatie/laravel-backup` dependency from composer
    
    * Publish config files for `laravel-backup`
    
    * Command programmed to execute backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean --disable-notifications')->daily()->at('01:00');
+        $schedule->command('backup:run --disable-notifications')->daily()->at('01:30');
     }
 
     /**
