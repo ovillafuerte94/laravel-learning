7bd8b5fb62b98c1d47f2a9a0ca5abb533ecc0122
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-01-30 15:30:55 -0500

    Preserve query string in pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        $this->app->resolving(LengthAwarePaginator::class, static function (LengthAwarePaginator $paginator) {
+            return $paginator->withQueryString();
+        });
+
+        $this->app->resolving(Paginator::class, static function (Paginator $paginator) {
+            return $paginator->withQueryString();
+        });
     }
 }
