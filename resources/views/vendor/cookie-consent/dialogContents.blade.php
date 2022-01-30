e49913df02040481e52d78dd20717ed69757c3d9
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-01-30 12:23:28 -0500

    Implement cookie consent (#5)
    * Added `spatie/laravel-cookie-consent` dependency from composer
    
    * Publish configuration file for `laravel-cookie-consent`
    
    * Publish lang files for `laravel-cookie-consent`
    
    * Include cookie consent in template
    
    * Publish dialog contents for cookie consent
    
    * Update dialog contents for cookie consent

---
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -0,0 +1,8 @@
+<div class="js-cookie-consent cookie-consent fixed-bottom rounded-top rounded-0 shadow-lg alert bg-info mb-0">
+    <span class="text-white align-middle cookie-consent__message">
+        {!! trans('cookie-consent::texts.message') !!}
+    </span>
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-sm btn-primary text-white float-end">
+        {{ trans('cookie-consent::texts.agree') }}</strong>
+    </button>
+</div>
