a603510b79ef04434e321f3e1b1eb2a65d0fcaf5
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-02-13 01:41:07 -0500

    Add `WhereNot` to query builder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\LengthAwarePaginator;
+use ProtoneMedia\LaravelEloquentWhereNot\WhereNot;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        WhereNot::addMacro();
         Paginator::useBootstrap();
 
         $this->app->resolving(LengthAwarePaginator::class, static function (LengthAwarePaginator $paginator) {
